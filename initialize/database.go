0fa9d7b4b5bba0ac42f00865193c0b35855d9b80
AlexProefex <102544315+AlexProefex@users.noreply.github.com>
2024-05-25 09:14:05 -0500

    update
    update

--- initialize/database.go
+++ initialize/database.go
@@ -2,39 +2,42 @@ package initialize
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
 	"unity/repository/migrations"
 
-	"github.com/lpernett/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
-func init() {
-	pwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-	err = godotenv.Load(filepath.Join(pwd, ".env"))
+/*
+	func init() {
+		pwd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+		err = godotenv.Load(filepath.Join(pwd, ".env"))
 
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 
 }
-
+*/
 func ConnectionDB() {
 	var err error
-	driver := os.Getenv("DRIVER")
-	host := os.Getenv("HOST")
-	user := os.Getenv("USER")
-	password := os.Getenv("PASSWORD")
-	dbName := os.Getenv("DBNAME")
-	dbPor := os.Getenv("PORT")
+	driver := "mysql"
+	//os.Getenv("DRIVER")
+	host := "127.0.0.1"
+	//os.Getenv("HOST")
+	user := "kiru"
+	//os.Getenv("USER")
+	password := "&I%g2o{icSqC"
+	//os.Getenv("PASSWORD")
+	dbName := "text"
+	//os.Getenv("DBNAME")
+	dbPor := "3306"
+	//os.Getenv("PORT")
 
 	url := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, dbPor, dbName)
 	fmt.Println(url + driver)
